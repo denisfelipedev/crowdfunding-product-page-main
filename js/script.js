96e41089fc40e93b5c57bbefe90b33fa351e6aab
Denis Felipe <denisfelipedev@gmail.com>
2021-03-19 12:48:35 -0300

    Responsividade Finalizada
    Menu finalizado e inciando o javascript

---
+++ js/script.js
@@ -0,0 +1,51 @@
+// function onClickMenu() {
+//     document.getElementById("menu-mobile").classList.toggle("show")
+//     document.getElementById("menu-mobile-close").classList.toggle("show")
+// }
+
+// const openMenu = document.querySelector('menu-hamb');
+// const closeMenu = document.querySelector('menu-close');
+// const navbar = document.querySelector('.navbar-menu');
+
+// openMenu.addEventListener('click', show);
+// closeMenu.addEventListener('click', close);
+
+function openMenu(){
+    const openMenu = document.querySelector('.menu-hamb');
+    const closeMenu = document.querySelector('.menu-close');
+    const navbar = document.querySelector('.navbar-menu');
+    const cardContainer = document.querySelector('.card-container');
+    const masterLogo = document.querySelector('.logo-mastercraft');
+    const paddingChanges = document.querySelector('.card1');
+    const hero = document.querySelector('.hero');
+
+    openMenu.classList.add('show');
+    closeMenu.classList.add('show');
+    navbar.classList.add('show');
+    cardContainer.classList.add('show');
+    masterLogo.classList.add('show');
+    paddingChanges.classList.add('show');
+    hero.classList.add('show');
+}
+
+function closeMenu(){
+    const openMenu = document.querySelector('.menu-hamb');
+    const closeMenu = document.querySelector('.menu-close');
+    const navbar = document.querySelector('.navbar-menu');
+    const cardContainer = document.querySelector('.card-container');
+    const masterLogo = document.querySelector('.logo-mastercraft');
+    const paddingChanges = document.querySelector('.card1');
+    const hero = document.querySelector('.hero');
+
+    openMenu.classList.remove('show');
+    closeMenu.classList.remove('show');
+    navbar.classList.remove('show');
+    cardContainer.classList.remove('show');
+    masterLogo.classList.remove('show');
+    paddingChanges.classList.remove('show');
+    hero.classList.remove('show');
+    
+}
+
+
+
